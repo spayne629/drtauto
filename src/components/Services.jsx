2c7221f83c9085a93e17e3d49ebb4550aa38943c
spayne629 <spayne629@gmail.com>
2024-11-23 01:46:45 -0500

    finished top row of cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,38 +12,71 @@ const Services = () => {
 
     return (
         <>
-            <Container fluid>
-                <Row xs={1} md={2} lg={3} xl={4} className="g-4 d-flex flex-row">
-                        <Card className="services">
-                            <Card.Header variant="top">
-                                <img src="../assets/images/asset22.png" alt="car and wrench icon" />
-                            </Card.Header>
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    blah blah blah
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                </Row>
-            </Container>
-            <Row xs={1} md={2} lg={3} xl={4} className={`g-4 ${theme === 'light' ? 'body-bg-light' : 'body-bg-dark'}`}>
-                <Col>
-                    <Card
-                        className="services"
-                        border="primary"
-                        style={{ width: '18rem'}}
-                    >
-                        <Card.Img variant="top" src="holder.js/100px160" />
+            <Container className="row1" fluid>
+                <Row xs={1} md={2} lg={3} xl={4} className="w-100 d-flex flex-row">
+                    <Card className="services" id="fifteen">
+                        <Card.Header variant="top">
+                            <img src="src/assets/images/Asset 22.png" alt="car and wrench icon" width="75" className="d-inline-flex justify-content-center card-pic"/>
+                        </Card.Header>
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title className="fs-4 py-2 d-flex justify-content-end">15 <span>+</span> Years Experience</Card.Title>
                             <Card.Text>
-                                blah blah blah
+                                Dr. T has been delivering top-notch automotive service with a commitment to excellence for over 15 years. Whether it's routine maintenance or complex repairs, you can count on Dr. T's expertise and dedication to keep your vehicle running smoothly. Trust your car with the best in the business!
                             </Card.Text>
                         </Card.Body>
                     </Card>
-                </Col>
-            </Row>
+                    <Card className="diagservices">
+                        <Card.Header variant="top">
+                            <img src="src/assets/images/Asset 15.png" alt="check engine light icon" width="75" height="60"                      className="d-inline-flex justify-content-center card-pic"
+                            id="cel" />
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title className="fs-4 py-2 d-flex justify-content-end">Thorough Diagnostics</Card.Title>
+                            <Card.Text>
+                                We pride ourselves on providing thorough diagnostic services to pinpoint any issues with your vehicle. Our state-of-the-art equipment and experienced technicians ensure that no problem goes undetected, giving you peace of mind and a smooth ride every time.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="services">
+                        <Card.Header variant="top">
+                            <img src="src/assets/images/Asset 21.png" alt="car and wrench icon" width="75" height="75" className="d-inline-flex justify-content-center card-pic"/>
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title className="fs-4 pt-2 pb-1">Suspension Repairs</Card.Title>
+                            <Card.Text>
+                                <br />
+                                When it comes to suspension repairs, Dr. T's has you covered. Our skilled technicians use the latest tools and techniques to ensure your vehicle's suspension system is in top condition, providing you with a smoother, safer ride.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
+            <Container>
+                <Row xs={1} md={2} lg={3} xl={4} className={`g-4 ${theme === 'light' ? 'body-bg-light' : 'body-bg-dark'}`}>
+                    <Card className="services">
+                        <Card.Header variant="top">
+                            <img src="src/assets/images/Asset 22.png" alt="car and wrench icon" width="75" height="75" className="d-inline-flex justify-content-center card-pic"/>
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title className="fs-4 py-2 d-flex justify-content-end">15 <span>+</span> Years Experience</Card.Title>
+                            <Card.Text>
+                                Dr. T has been delivering top-notch automotive service with a commitment to excellence for over 15 years. Whether it's routine maintenance or complex repairs, you can count on Dr. T's expertise and dedication to keep your vehicle running smoothly. Trust your car with the best in the business!
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="services">
+                        <Card.Header variant="top">
+                            <img src="src/assets/images/Asset 22.png" alt="car and wrench icon" width="75" height="75" className="d-inline-flex justify-content-center card-pic"/>
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title className="fs-4 py-2 d-flex justify-content-end">15 <span>+</span> Years Experience</Card.Title>
+                            <Card.Text>
+                                Dr. T has been delivering top-notch automotive service with a commitment to excellence for over 15 years. Whether it's routine maintenance or complex repairs, you can count on Dr. T's expertise and dedication to keep your vehicle running smoothly. Trust your car with the best in the business!
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
         </>
     );
 };
